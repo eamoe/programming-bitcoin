c39422be8fba10abe682c5e5b42d2d6528ee22aa
Eugene Menski <eugene.menski@gmail.com>
2023-04-01 14:18:54 +0400

    Freezed requirements

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
+from FieldElement import FieldElement
 import constants
-print(constants.N * constants.G)
+# print(constants.N * constants.G)
+
+from Point import *
+
+a = Point(-1, -1, 5, 7)
+a2 = a + a
+
+print(None * 0)
