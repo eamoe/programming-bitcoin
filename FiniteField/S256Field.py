c6847615a5a3dd0d99c707d73f5ddfa46d7a0952
Eugene Menski <eugene.menski@gmail.com>
2023-04-01 18:17:02 +0400

    Added comments to S256Field class

--- FiniteField/S256Field.py
+++ FiniteField/S256Field.py
@@ -5,10 +5,12 @@ import constants
 # Redefine FieldElement class for working with secp256k1 curve
 class S256Field(FieldElement):
 
+    # Subclassing the FieldElement class in order not to pass in P all the time
     def __init__(self, num, prime=None):
         super().__init__(num=num, prime=constants.P)
 
     def __repr__(self):
+        # Display a 256-bit number consistently by filling 64 characters to see any leading zeros
         return '{:x}'.format(self.num).zfill(64)
 
     # tag::source2[]
