5cb8aa62a7001686c0dd0c558d45502aaf16285d
Eugene Menski <eugene.menski@gmail.com>
2023-04-03 18:16:36 +0400

    Added op, script, TxIn

---
+++ TxIn.py
@@ -0,0 +1,59 @@
+from script import Script
+
+from Helper.helper import (
+    int_to_little_endian,
+    little_endian_to_int
+)
+
+from TxFetcher import TxFetcher
+
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig=None, sequence=0xffffffff):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        if script_sig is None:  # <1>
+            self.script_sig = Script()
+        else:
+            self.script_sig = script_sig
+        self.sequence = sequence
+
+    def __repr__(self):
+        return '{}:{}'.format(
+            self.prev_tx.hex(),
+            self.prev_index,
+        )
+    # end::source2[]
+
+    @classmethod
+    def parse(cls, s, testnet=False):
+        version = little_endian_to_int(s.read(4))
+        return cls(version, None, None, None, testnet=testnet)
+
+    # tag::source5[]
+    def serialize(self):
+        '''Returns the byte serialization of the transaction input'''
+        result = self.prev_tx[::-1]
+        result += int_to_little_endian(self.prev_index, 4)
+        result += self.script_sig.serialize()
+        result += int_to_little_endian(self.sequence, 4)
+        return result
+    # end::source5[]
+
+    # tag::source8[]
+    def fetch_tx(self, testnet=False):
+        return TxFetcher.fetch(self.prev_tx.hex(), testnet=testnet)
+
+    def value(self, testnet=False):
+        '''Get the output value by looking up the tx hash.
+        Returns the amount in satoshi.
+        '''
+        tx = self.fetch_tx(testnet=testnet)
+        return tx.tx_outs[self.prev_index].amount
+
+    def script_pubkey(self, testnet=False):
+        '''Get the ScriptPubKey by looking up the tx hash.
+        Returns a Script object.
+        '''
+        tx = self.fetch_tx(testnet=testnet)
+        return tx.tx_outs[self.prev_index].script_pubkey
+    # end::source8[]
