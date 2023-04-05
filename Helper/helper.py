7c0c852f358996e60016abc2603640848fad11ba
Eugene Menski <eugene.menski@gmail.com>
2023-04-05 22:31:34 +0400

    Fixed methods

--- Helper/helper.py
+++ Helper/helper.py
@@ -94,9 +94,9 @@ def h160_to_p2pkh_address(h160, testnet=False):
         prefix = b'\x00'
     return encode_base58_checksum(prefix + h160)
 
-def h160_to_p2pkh_address(h160, testnet=False):
+def h160_to_p2sh_address(h160, testnet=False):
     if testnet:
-        prefix = b'\x6f'
+        prefix = b'\xc4'
     else:
-        prefix = b'\x00'
+        prefix = b'\x05'
     return encode_base58_checksum(prefix + h160)
