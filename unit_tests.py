5a122eacf2a6cee6b82d4fff2edae5c52885a773
Eugene Menski <eugene.menski@gmail.com>
2023-04-05 22:41:35 +0400

    reworked unit tests imports

--- unit_tests.py
+++ unit_tests.py
@@ -1,15 +1,15 @@
 import unittest
 
+from Tests.ecc_test import *
 from Tests.FieldElement_test import *
-from Tests.Point_test import *
-from Tests.S256_test import *
 from Tests.helper_test import *
-from Tests.Tx_test import *
+from Tests.op_test import *
+from Tests.Point_test import *
+from Tests.PrivateKey_test import *
+from Tests.S256_test import *
 from Tests.script_test import *
 from Tests.Signature_test import *
-from Tests.PrivateKey_test import *
-from Tests.ecc_test import *
-from Tests.op_test import *
+from Tests.Tx_test import *
 
 unittest.main()
 
