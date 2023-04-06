2e63f3cfc69216f5859edadb3b7df69b88710cb4
Eugene Menski <eugene.menski@gmail.com>
2023-04-06 16:03:39 +0400

    Extended script module

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from logging import getLogger
 
 from Helper.helper import (
@@ -7,6 +8,9 @@ from Helper.helper import (
     read_varint,
 )
 from op import (
+    op_equal,
+    op_hash160,
+    op_verify,
     OP_CODE_FUNCTIONS,
     OP_CODE_NAMES,
 )
@@ -165,6 +169,26 @@ class Script:
             else:
                 # add the cmd to the stack
                 stack.append(cmd)
+                if len(cmds) == 3 and cmds[0] == 0xa9 \
+                    and type(cmds[1]) == bytes and len(cmds[1]) == 20 \
+                    and cmds[2] == 0x87:
+                    # we execute the next three opcodes
+                    cmds.pop()
+                    h160 = cmds.pop()
+                    cmds.pop()
+                    if not op_hash160(stack):
+                        return False
+                    stack.append(h160)
+                    if not op_equal(stack):
+                        return False
+                    # final result should be a 1
+                    if not op_verify(stack):
+                        LOGGER.info('bad p2sh h160')
+                        return False
+                    # hashes match! now add the RedeemScript
+                    redeem_script = encode_varint(len(cmd)) + cmd
+                    stream = BytesIO(redeem_script)
+                    cmds.extend(Script.parse(stream).cmds)
         if len(stack) == 0:
             return False
         if stack.pop() == b'':
