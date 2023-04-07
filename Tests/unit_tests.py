9885480a508115e85fdcb2e0c5fe2e28b3b82a1c
Eugene Menski <eugene.menski@gmail.com>
2023-04-07 17:14:29 +0400

    Renamed tests

--- Tests/unit_tests.py
+++ Tests/unit_tests.py
@@ -2,18 +2,18 @@ import unittest
 
 from Tests.block_test import *
 from Tests.ecc_test import *
-from Tests.FieldElement_test import *
+from Tests.field_element_test import *
 from Tests.helper_test import *
-from Tests.merkleblock_test import *
-from Tests.merkletree_test import *
+from Tests.merkle_block_test import *
+from Tests.merkle_tree_test import *
 from Tests.network_test import *
 from Tests.op_test import *
-from Tests.Point_test import *
-from Tests.PrivateKey_test import *
-from Tests.S256_test import *
+from Tests.point_test import *
+from Tests.private_key_test import *
+from Tests.s256_test import *
 from Tests.script_test import *
-from Tests.Signature_test import *
-from Tests.Tx_test import *
+from Tests.signature_test import *
+from Tests.tx_test import *
 
 unittest.main()
 
